d0e4504ce451d70a6aeb6965d540c7eecd5178bd
zoe <zoe@example.com>
2025-02-23 21:44:31 +0800

    up

--- eval.sh
+++ eval.sh
@@ -3,8 +3,8 @@ NUM_GPUS=3
 MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
 MODEL_ARGS="pretrained=$MODEL,dtype=float16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilisation=0.8,generation_parameters={temperature:0.6,top_p:0.95}"
 #MODEL_ARGS="pretrained=$MODEL,dtype=float16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilisation=0.8"
-TASK=aime24
-#TASK=math_500
+#TASK=aime24
+TASK=math_500
 OUTPUT_DIR=data/evals/$MODEL
 
 lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
