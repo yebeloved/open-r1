d8a50649ea1f4bcc7f373d91ffb621c018cfc05d
zoe <zoe@example.com>
2025-02-23 20:17:37 +0800

    up

--- eval.sh
+++ eval.sh
@@ -7,15 +7,15 @@ TASK=aime24
 #TASK=math_500
 OUTPUT_DIR=data/evals/$MODEL
 
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR 
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR 
 
 
 lighteval vllm \
-    "config.yaml" \
+    "eval_config.yaml" \
     "custom|$TASK|0|0" \
     --custom-tasks tasks.py \
     --use-chat-template \
-    --output-dir ./results/
+    --output-dir $OUTPUT_DIR
