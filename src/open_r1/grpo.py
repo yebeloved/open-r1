67420cd84e774e86e9a38af8e03fe4a433473ce5
zoe <zoe@example.com>
2025-02-24 11:45:06 +0800

    up

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -149,6 +149,7 @@ def main(script_args, training_args, model_args):
 
     # Load the dataset
     dataset = load_dataset(script_args.dataset_name, name=script_args.dataset_config)
+    dataset['train'] = dataset['train'].select(range(10))
 
     ################
     # Load tokenizer
